c7aa09f315c05e564b5e6d5e0ea6069fb1d5f045
HeleneMG <helenemouttetg@gmail.com>
2019-09-20 17:02:19 +0200

    carte

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//MAP
+
+var mymap = L.map('map', {
+    center: [41.888634, -87.628091],
+    zoom: 8,
+    zoomControl: false,
+});
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWNhcmRuaWNvbGFzOTEiLCJhIjoiY2swcnl3Ymt4MDFpMTNkcDVrdzRvMmQ5MyJ9.Jmw2gfO6CTF4yjmOkGm49w', {
+    maxZoom: 20,
+    attribution: 'Données &copy; Contributeurs <a href="http://openstreetmap.org">OpenStreetMap</a> + ' +
+        '<a href="http://mapbox.com">Mapbox</a> | ' +
+        '<a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a> ' +
+        'Guillaume Rouan 2016',
+    id: 'mapbox.streets'
+}).addTo(mymap);
+L.marker([41.888634, -87.628091]).addTo(mymap); 
+
+
+//LISTE RESTAUS
+
 const listOfRestaurant = document.querySelector('#liste');
 const userClick = document.querySelector('#userSend');
 const userInput = document.querySelector('#userInput');
@@ -9,9 +29,9 @@ const getResto = (city) => {
         .then(response => response.json())
         .then((data) => {
             console.log(data.restaurants);
+            liste.innerHTML = '';
             data.restaurants.forEach(element => {
-                console.log(element.name);
-                listOfRestaurant.insertAdjacentHTML('beforeend', `
+                listOfRestaurant.insertAdjacentHTML('beforeend', `                   
                     <li>
                         <div class="card mb-3 shadow-sm p-3 mb-5 bg-white rounded carte" style="max-width: 540px;">
                             <div class="row no-gutters">
@@ -20,7 +40,7 @@ const getResto = (city) => {
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body">
-                                        <h5 class="card-title">${element.name}</h5>
+                                        <h5 class="card-title"><a href="#">${element.name}</a></h5>
                                         <ul>
                                             <li>Price range : ${element.price}</li>
                                             <li>Phone : ${element.phone}</li>
@@ -36,29 +56,26 @@ const getResto = (city) => {
         })
         .catch((error) => {
             console.log(error.message);
+        });
+    const URLVille = `https://nominatim.openstreetmap.org/q=${city}&format=json`;
+    fetch(URLVille)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.lat, data.lng);
+            liste.innerHTML = '';
+            data.restaurants.forEach(element => {
+                console.log(element.lat, element.lng);
+            });
+        })
+        .catch((error) => {
+            console.log(error.message);
         })
 }
-getResto("seattle");
+getResto("chicago");
 
 userClick.addEventListener('click', (event) => {
     event.preventDefault();
     getResto(userInput.value);
 });
 
-var mymap = L.map('map', { 
-    center: [51.505, -0.09], 
-    zoom: 17, 
-    zoomControl: false, 
-    scrollWheelZoom: false 
-});
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWNhcmRuaWNvbGFzOTEiLCJhIjoiY2swcnl3Ymt4MDFpMTNkcDVrdzRvMmQ5MyJ9.Jmw2gfO6CTF4yjmOkGm49w', { 
-    maxZoom: 20, 
-    attribution: 'Données &copy; Contributeurs <a href="http://openstreetmap.org">OpenStreetMap</a> + ' +
-        '<a href="http://mapbox.com">Mapbox</a> | ' +
-        '<a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a> ' +
-        'Guillaume Rouan 2016', 
-    id: 'mapbox.streets' 
-}).addTo(mymap);
-
-L.marker([51.505, -0.09]).addTo(mymap); 
